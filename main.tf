1948f2b2e17aa54cfd16fd6512c20bd88d58781e
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-27 03:55:15 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "restapi" {
+  alias                = "west"
+  uri                  = "http://192.168.1.93:32560/_slm"
+  debug                = true
+  headers              = {"Content-Type" = "application/json"}
+  write_returns_object = true
+  insecure             = true
+  id_attribute         = "/"
+  create_method        = "PUT"
+  update_method        = "PUT"
+  destroy_method       = "PUT"
+}
+
+resource "restapi_object" "create_policy" {
+  depends_on = [restapi_object.create_repository]
+  object_id = "sspolicy"
+  path = "/policy/daily-snapshots"
+  data = "\"name\": \"weather-data\", \"snapshotName\": \"weather-data-policy1\", \"schedule\": \"0 0 0 * * ?\", \"repository\": \"eck-ss\", \"config\":{\"indices\": [\"weather-data-2016\"]}, \"retention\": {\"expireAfterUnit\": \"d\"}, \"isManagedPolicy\": false}"
+}
+
+
+resource "restapi_object" "exec_policy" {
+  depends_on = [restapi_object.create_policy]
+  object_id = "sspolicyexec"
+  path = "/policy/daily-snapshots/_execute"
+  data = ""
+}

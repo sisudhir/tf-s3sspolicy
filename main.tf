6ad38f7a200491587879c97840b902d274531f96
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-27 04:45:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,8 +25,21 @@ resource "restapi_object" "create_policy" {
   data = "{ \"name\":\"weather-data\", \"snapshotName\":\"weather-data-policy1\", \"schedule\":\"0 0 0 * * ?\", \"repository\":\"eck-ss\",  \"config\":{ \"indices\":[\"weather-data-2016\"] }, \"retention\":{ \"expireAfterUnit\":\"d\" }, \"isManagedPolicy\":\"false\" }" 
 }
 
+provider "restapi" {
+  alias                = "rest"
+  uri                  = "http://192.168.1.93:32560/_slm/policy"
+  debug                = true
+  headers              = {"Content-Type" = "application/json"}
+  write_returns_object = true
+  insecure             = true
+  id_attribute         = "/"
+  create_method        = "PUT"
+  update_method        = "PUT"
+  destroy_method       = "PUT"
+}
 
 resource "restapi_object" "exec_policy" {
+  provider = restapi.rest
   depends_on = [restapi_object.create_policy]
   object_id = "sspolicyexec"
   path = "/daily-snapshots/_execute"

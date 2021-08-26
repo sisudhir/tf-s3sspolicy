49ab37c46de04aa1cb19d81efb96f104966fedc2
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-27 04:11:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,8 @@ provider "restapi" {
 resource "restapi_object" "create_policy" {
   object_id = "sspolicy"
   path = "/policy/daily-snapshots"
-  data = "{ \"name\":\"weather-data\", \"snapshotName\":\"weather-data-policy1\", \"schedule\":\"0 0 0 * * ?\", \"repository":\"eck-ss\",  \"config":{ \"indices\":[\"weather-data-2016\"] }, \"retention\":{ \"expireAfterUnit\":\"d\" }, \"isManagedPolicy\":\"false\" }" }
+  data = "{ \"name\":\"weather-data\", \"snapshotName\":\"weather-data-policy1\", \"schedule\":\"0 0 0 * * ?\", \"repository":\"eck-ss\",  \"config":{ \"indices\":[\"weather-data-2016\"] }, \"retention\":{ \"expireAfterUnit\":\"d\" }, \"isManagedPolicy\":\"false\" }" 
+}
 
 
 resource "restapi_object" "exec_policy" {

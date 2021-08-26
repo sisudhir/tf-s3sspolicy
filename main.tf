b5571940e2fc58420f27fbe386e4d347682feb63
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-27 05:13:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,26 +7,26 @@ terraform {
   }
 }
 
-#provider "restapi" {
-  #uri                  = "http://192.168.1.93:32560/_slm/policy"
-  #debug                = true
-  #headers              = {"Content-Type" = "application/json"}
-  #write_returns_object = true
-  #insecure             = true
-  #id_attribute         = "/"
-  #create_method        = "PUT"
-  #update_method        = "PUT"
-  #destroy_method       = "PUT"
-#}
+provider "restapi" {
+  uri                  = "http://192.168.1.93:32560/_slm/policy"
+  debug                = true
+  headers              = {"Content-Type" = "application/json"}
+  write_returns_object = true
+  insecure             = true
+  id_attribute         = "/"
+  create_method        = "PUT"
+  update_method        = "PUT"
+  destroy_method       = "PUT"
+}
 
-#resource "restapi_object" "create_policy" {
-  #object_id = "sspolicy"
-  #path = "/daily-snapshots"
-  #data = "{ \"name\":\"weather-data\", \"snapshotName\":\"weather-data-policy1\", \"schedule\":\"0 0 0 * * ?\", \"repository\":\"eck-ss\",  \"config\":{ \"indices\":[\"weather-data-2016\"] }, \"retention\":{ \"expireAfterUnit\":\"d\" }, \"isManagedPolicy\":\"false\" }" 
-#}
+resource "restapi_object" "create_policy" {
+  object_id = "sspolicy"
+  path = "/daily-snapshots"
+  data = "{ \"name\":\"weather-data\", \"snapshotName\":\"weather-data-policy1\", \"schedule\":\"0 0 0 * * ?\", \"repository\":\"eck-ss\",  \"config\":{ \"indices\":[\"weather-data-2016\"] }, \"retention\":{ \"expireAfterUnit\":\"d\" }, \"isManagedPolicy\":\"false\" }" 
+}
 
 provider "restapi" {
-  #alias                = "rest"
+  alias                = "rest"
   uri                  = "http://192.168.1.93:32560/_slm/policy/daily-snapshots/_execute"
   debug                = true
   headers              = {"Content-Type" = "application/json"}

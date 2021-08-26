bcf85923ff5e2f998a9033462c31459d1ec491b4
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-27 04:33:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "restapi" {
-  uri                  = "http://192.168.1.93:32560/_slm"
+  uri                  = "http://192.168.1.93:32560/_slm/policy"
   debug                = true
   headers              = {"Content-Type" = "application/json"}
   write_returns_object = true
@@ -21,7 +21,7 @@ provider "restapi" {
 
 resource "restapi_object" "create_policy" {
   object_id = "sspolicy"
-  path = "/policy/daily-snapshots"
+  path = "/daily-snapshots"
   data = "{ \"name\":\"weather-data\", \"snapshotName\":\"weather-data-policy1\", \"schedule\":\"0 0 0 * * ?\", \"repository\":\"eck-ss\",  \"config\":{ \"indices\":[\"weather-data-2016\"] }, \"retention\":{ \"expireAfterUnit\":\"d\" }, \"isManagedPolicy\":\"false\" }" 
 }
 

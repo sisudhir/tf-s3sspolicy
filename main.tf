9a1e5b40c563634ffdf9b01d79807b070a9fb000
Sindhu Sudhir <sisudhir@cisco.com>
2021-09-11 14:15:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,27 +22,6 @@ provider "restapi" {
 resource "restapi_object" "create_policy" {
   object_id = "sspolicy"
   path = "/daily-snapshots"
-  data = "{ \"name\":\"weather-data\", \"snapshotName\":\"weather-data-policy1\", \"schedule\":\"0 0 0 * * ?\", \"repository\":\"${base_path}\",  \"config\":{ \"indices\":[\"weather-data-2016\"] }, \"retention\":{ \"expireAfterUnit\":\"d\" }, \"isManagedPolicy\":\"false\" }" 
+  data = "{ \"name\":\"${var.data_name}\", \"snapshotName\":\"weather-data-policy1\", \"schedule\":\"0 0 0 * * ?\", \"repository\":\"${var.base_path}\",  \"config\":{ \"indices\":[\"weather-data-2016\"] }, \"retention\":{ \"expireAfterUnit\":\"d\" }, \"isManagedPolicy\":\"false\" }" 
 }
 
-/*provider "restapi" {
-  #alias                = "rest"
-  uri                  = "http://192.168.1.93:32560/_slm/policy/daily-snapshots/_execute"
-  debug                = true
-  headers              = {"Content-Type" = "application/json"}
-  create_returns_object = false
-  write_returns_object = false
-  insecure             = true
-  id_attribute         = "/"
-  create_method        = "POST"
-  update_method        = "POST"
-  destroy_method       = "POST"
-}*/
-
-/*resource "restapi_object" "exec_policy" {
-  #provider = restapi.rest
-  depends_on = [restapi_object.create_policy]
-  object_id = "execpolicy"
-  path = "/daily-snapshots/_execute"
-  data = ""
-}*/

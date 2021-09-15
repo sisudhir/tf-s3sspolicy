371063c43766082a69e075faec356cdf810a1c18
Sindhu Sudhir <sisudhir@cisco.com>
2021-09-15 07:28:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,15 @@
 variable "eckmstr_uri" {
   type = string
   description = "Elastic master ip and port"
-  default = "192.168.1.93:32560"
 }
 
 variable "base_path" {
   type = string
   description = "ECK snapshot Name"
-  default = "eck-ss"
 }
 
 variable "data_name" {
   type = string
   description = "Rally track NOAA data name"
-  default = "weather-data"
 }
 

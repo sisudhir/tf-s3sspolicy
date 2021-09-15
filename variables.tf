631c6e51471ed77f8db1721da3689c65b96de30c
Sindhu Sudhir <sisudhir@cisco.com>
2021-09-15 20:37:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "eckmstr_uri" {
-  type = string
-  description = "Elastic master ip and port"
+type = string
+description = "Elastic master ip and port"
 }
 
 variable "base_path" {
-  type = string
-  description = "ECK snapshot Name"
+type = string
+description = "ECK snapshot Name"
 }
 
 variable "data_name" {
-  type = string
-  description = "Rally track NOAA data name"
+type = string
+description = "Rally track NOAA data name"
 }
 
